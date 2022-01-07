ad744326e5a746e8a7ea53e7051726bf4bfeb2a4
Jonghoon Jang <jayjang0209@gmail.com>
2022-01-06 22:54:48 -0800

    Fix bug

--- src/strings/P125/ReverseInteger.java
+++ src/strings/P125/ReverseInteger.java
@@ -1,5 +1,7 @@
 package strings.P125;
 
+import javax.print.DocFlavor;
+
 /**
  * LeetCode P7. Reverse Integer
  *
@@ -9,5 +11,35 @@ package strings.P125;
 public final class ReverseInteger {
 
     private ReverseInteger() {
+    }
+
+    public static int reverse(final int x) {
+        if (x < -Math.pow(2, 31) || x > Math.pow(2, 31) - 1) {
+            return 0;
+        }
+
+        String stringValue = String.valueOf(x);
+        StringBuilder reversedValue = new StringBuilder();
+        System.out.println(stringValue);
+        int numberLength = stringValue.length();
+        for (int i = numberLength - 1; i >= 0; i--) {
+//            System.out.println(stringValue.charAt(i));
+            if (stringValue.charAt(i) != '-') {
+                reversedValue.append(stringValue.charAt(i));
+            }
+        }
+        int reversedInt = Integer.parseInt(String.valueOf(reversedValue));
+        reversedInt = stringValue.charAt(0) == '-' ? -reversedInt : reversedInt;
+
+        if (reversedInt < -Math.pow(2, 31) || reversedInt > Math.pow(2, 31) - 1) {
+            return 0;
+        }
+
+        return reversedInt;
+    }
+
+    public static void main(String[] args) {
+        int test = 123;
+        System.out.println(reverse(test));
     }
 }
