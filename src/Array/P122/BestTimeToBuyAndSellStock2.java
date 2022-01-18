8f95737d821d691f0574d43dea4a47fb0aa34fc5
Jonghoon Jang <jayjang0209@gmail.com>
2022-01-17 19:14:53 -0800

    Add LeetCode P122 class

---
+++ src/Array/P122/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,11 @@
+package Array.P122;
+
+public class BestTimeToBuyAndSellStock2 {
+
+    private BestTimeToBuyAndSellStock2() {
+    }
+
+    public static int maxProfit(int[] prices) {
+        return 0;
+    }
+}
