af104560bd7ab95a3a853863970563fb25e5939d
Jonghoon Jang <jayjang0209@gmail.com>
2022-04-20 22:06:06 -0700

    Add javadocs

--- src/graph/dfs/p547/NumberOfProvinces.java
+++ src/graph/dfs/p547/NumberOfProvinces.java
@@ -1,13 +1,31 @@
-package Graph.DFS.P547;
+package graph.dfs.p547;
 
 import java.util.Stack;
 
-public class NumberOfProvinces {
+/**
+ * LeetCode P547. Number of Provinces
+ *
+ * @author Jay Jang
+ * @version Apr, 2022
+ */
+public final class NumberOfProvinces {
 
     private NumberOfProvinces() {
     }
 
-    public static int findCircleNum(int[][] isConnected) {
+    /**
+     * Return the total number of provinces.
+     *
+     * a province: a group of directly or indirectly connected cities
+     * and no other cities outside of the group
+     *
+     * @param isConnected as 2D array representing n x n matrix
+     * @return total number of provinces as an int
+     *
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     */
+    public static int findCircleNum(final int[][] isConnected) {
         int totalNumberOfProvinces = 0;
 
         int numberOfNode = isConnected.length;
@@ -17,14 +35,9 @@ public class NumberOfProvinces {
             if (nodeVisited[v] == 0) {
                 Stack<Integer> stack = new Stack<>();
                 stack.push(v);
-
                 while (!stack.empty()) {
-
                     int vtx = stack.pop();
-
                     nodeVisited[vtx] = 1;
-                    System.out.println(vtx + "visited");
-
                     int[] adjList = isConnected[vtx];
                     for (int j = 0; j < numberOfNode; j++) {
                         int u = adjList[j];
@@ -39,28 +52,21 @@ public class NumberOfProvinces {
         return totalNumberOfProvinces;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Runs the program.
+     *
+     * @param args not used
+     */
+    public static void main(final String[] args) {
         int[][] test1 = new int[3][3];
-        test1[0][0] = 1;
-        test1[0][1] = 1;
-        test1[0][2] = 0;
-        test1[1][0] = 1;
-        test1[1][1] = 1;
-        test1[1][2] = 0;
-        test1[2][0] = 0;
-        test1[2][1] = 0;
-        test1[2][2] = 1;
+        test1[0] = new int[]{1, 1, 0};
+        test1[1] = new int[]{1, 1, 0};
+        test1[2] = new int[]{0, 0, 1};
 
         int[][] test2 = new int[3][3];
-        test2[0][0] = 1;
-        test2[0][1] = 0;
-        test2[0][2] = 0;
-        test2[1][0] = 0;
-        test2[1][1] = 1;
-        test2[1][2] = 0;
-        test2[2][0] = 0;
-        test2[2][1] = 0;
-        test2[2][2] = 1;
+        test1[0] = new int[]{1, 0, 0};
+        test1[1] = new int[]{0, 1, 0};
+        test1[2] = new int[]{0, 0, 1};
 
         int[][] test3 = new int[4][4];
         test3[0] = new int[]{1, 0, 0, 1};
@@ -69,9 +75,8 @@ public class NumberOfProvinces {
         test3[3] = new int[]{1, 0, 1, 1};
 
 
-        System.out.println(findCircleNum(test1));
-        System.out.println(findCircleNum(test2));
-        int r = findCircleNum(test3);
-        System.out.println(r);
+        System.out.println("number of provinces of test1: " + findCircleNum(test1));
+        System.out.println("number of provinces of test2: " + findCircleNum(test2));
+        System.out.println("number of provinces of test3: " + findCircleNum(test3));
     }
 }
