e54e0b3042873ba0c2e584efd385fe1db31846dc
Jonghoon Jang <jayjang0209@gmail.com>
2022-04-23 23:01:33 -0700

    Add javadocs

--- src/graph/unionfind/p547/NumberOfConnectedComponents.java
+++ src/graph/unionfind/p547/NumberOfConnectedComponents.java
@@ -1,22 +1,39 @@
-package graph.unionfind.p323;
+package graph.unionfind.p547;
 
 import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
 
+/**
+ * LeetCode P547. Number of Provinces
+ *
+ * @author Jay Jang
+ * @version Apr, 2022
+ */
 public class NumberOfConnectedComponents {
 
     public NumberOfConnectedComponents() {
     }
 
+    /**
+     * Return the total number of provinces using Iterative DFS.
+     *
+     * a province: a group of directly or indirectly connected cities
+     * and no other cities outside of the group
+     *
+     * @param isConnected as 2D array representing n x n matrix
+     * @return total number of provinces as an int
+     *
+     * Used Algorithm: Union-Find
+     */
     public static int findNumberOfConnectedComponents(final int[][] isConnected) {
         int numberOfNode = isConnected.length;
-        int[] parent = new int[numberOfNode];
 
+        // set the parent of each vertex to itself
+        int[] parent = new int[numberOfNode];
         for (int i = 0; i < numberOfNode; i++) {
             parent[i] = i;
         }
 
+        // Iterate through the adjacency matrix and union two vertices if they are connected
         for (int i = 0; i < numberOfNode; i++) {
             for (int j = 0; j < numberOfNode; j++) {
                 int[] adjList = isConnected[i];
@@ -26,28 +43,50 @@ public class NumberOfConnectedComponents {
             }
         }
 
+        // Create set to hold unique top most parents
         HashSet<Integer> uniqueParent = new HashSet<>();
 
+        // Traverse all vertices and insert all topmost parents
         for (int j = 0; j < numberOfNode; j++) {
             uniqueParent.add(findParent(parent, j));
         }
 
+        // Return the size unique parents, which is the number of connected components
         return uniqueParent.size();
     }
 
-    public static int findParent(int[] parent, int node) {
+    /**
+     * Find the topmost parent of a given node.
+     *
+     * @param parent an array containing the parent of each vertex
+     * @param node an int representing a vertex
+     */
+    public static int findParent(final int[] parent, int node) {
         while( node != parent[node]) {
             node = parent[node];
         }
         return node;
     }
 
+    /**
+     * Join first set containing the first node and second set containing the second node
+     * into a single subset.
+     *
+     * @param parent an array containing the parent of each vertex
+     * @param firstNode an int representing a vertex
+     * @param secondNode an int representing a vertex
+     */
     public static void union(int[] parent, int firstNode, int secondNode) {
         int firstParent = findParent(parent, firstNode);
         int secondParent = findParent(parent, secondNode);
         parent[firstParent] = secondParent;
     }
 
+    /**
+     * Runs the program.
+     *
+     * @param args not used
+     */
     public static void main(String[] args) {
         int[][] test1 = new int[3][3];
         test1[0] = new int[]{1, 1, 0};
