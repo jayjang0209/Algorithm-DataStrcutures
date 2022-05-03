e397ffdb4830bf24ff741ca4c3ed1d387eb86ba1
Jonghoon Jang <jayjang0209@gmail.com>
2022-05-02 23:03:40 -0700

    Add In order traversal solution

--- src/tree/binaryTree/traversal/binaryTreeInOrderTraversal.java
+++ src/tree/binaryTree/traversal/binaryTreeInOrderTraversal.java
@@ -9,7 +9,20 @@ import java.util.List;
 public class binaryTreeInOrderTraversal {
 
 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        LinkedList<Integer> inOrderTraversalNodes = new LinkedList<>();
+        inOrderTraversal(root, inOrderTraversalNodes);
+        return inOrderTraversalNodes;
+    }
 
+    public void inOrderTraversal(TreeNode root, LinkedList<Integer> inOrderNodes) {
+        if (root == null) {
+            return;
+        }
+        inOrderTraversal(root.getLeft(), inOrderNodes);
+        inOrderNodes.addLast(root.getVal());
+        inOrderTraversal(root.getRight(), inOrderNodes);
+    }
 
 }
 
