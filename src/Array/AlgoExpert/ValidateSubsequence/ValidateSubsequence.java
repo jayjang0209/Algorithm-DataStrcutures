cc06d43630a56f10b0178ff00d2e2b3a6b11a613
jayjang0209 <77429796+jayjang0209@users.noreply.github.com>
2022-08-08 22:03:51 -0700

    Add Javadoc

--- src/Array/AlgoExpert/ValidateSubsequence/ValidateSubsequence.java
+++ src/Array/AlgoExpert/ValidateSubsequence/ValidateSubsequence.java
@@ -4,8 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ValidateSubsequence {
+/**
+ * AlgoExpert Validate Subsequence.
+ *
+ * https://www.algoexpert.io/questions/validate-subsequence
+ * @author Jay Jang
+ * @version Aug, 2022
+ */
+public final class ValidateSubsequence {
 
+    private ValidateSubsequence() {
+    }
+
+    /**
+     * Given two non-empty arrays of integers, write a function that determines whether
+     * the second array is a subsequence of the first one.
+     *
+     * Sample
+     * array = [5, 1, 22, 25, 6, -1, 8, 10]
+     * sequence = [1, 6, -1, 10]
+     *
+     * @param array an array
+     * @param sequence an array
+     * @return true if the second array is a subsequence of the first one, otherwise return false.
+     *
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     */
     public static boolean isValidSubsequence(final List<Integer> array, final List<Integer> sequence) {
 
         if (array.size() == 1 && sequence.contains(array.get(0))) {
@@ -26,7 +51,7 @@ public class ValidateSubsequence {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10));
         ArrayList<Integer> sequence = new ArrayList<Integer>(Arrays.asList(22, 25, 6));
         System.out.println(isValidSubsequence(array, sequence));
